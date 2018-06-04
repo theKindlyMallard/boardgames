443ed9cc4bcb1017ceccde5e7f5b2702dd910d6b
theKindlyMallard <the.kindly.mallard@gmail.com>
2018-06-04 07:18:28 +0200

    Add game saving & tiny view changes.

---
+++ application/Controller/GameController.php
@@ -0,0 +1,178 @@
+<?php
+
+namespace Controller;
+
+/**
+ * Game controller.
+ * 
+ * @author theKindlyMallard <the.kindly.mallard@gmail.com>
+ */
+class GameController extends Controller {
+    
+    /**
+     * @var string Name of form field complexity.
+     */
+    const FIELD_COMPLEXITY = 'gameComplexity';
+    /**
+     * @var string Name of form field description.
+     */
+    const FIELD_DESCRIPTION = 'gameDescription';
+    /**
+     * @var string Name of form field image.
+     */
+    const FIELD_IMAGE = 'gameImage';
+    /**
+     * @var string Name of form field play time.
+     */
+    const FIELD_PLAY_TIME = 'gamePlayTime';
+    /**
+     * @var string Name of form field players number.
+     */
+    const FIELD_PLAYERS_NUMBER = 'gamePlayersNumber';
+    /**
+     * @var string Name of form field publisher.
+     */
+    const FIELD_PUBLISHER = 'gamePublisher';
+    /**
+     * @var string Name of form field name.
+     */
+    const FIELD_NAME = 'gameName';
+    /**
+     * @var string Name of form field site URL.
+     */
+    const FIELD_SITE_URL = 'gameSiteUrl';
+    /**
+     * @var string Name of form field type.
+     */
+    const FIELD_TYPE = 'gameType';
+    
+    /**
+     * @var string Name of game add form.
+     */
+    const FORM_NAME_GAME_ADD = 'gameAdd';
+    
+    /**
+     * @var \Model\GameModel Default model for this controller. 
+     */
+    protected $model;
+    
+    /**
+     * @author theKindlyMallard <the.kindly.mallard@gmail.com>
+     */
+    public function __construct(bool $loadModel = true) {
+        parent::__construct($loadModel);
+        
+        session_id(UserController::SESSION_ID_USER_SIGN_IN);
+        session_start();
+    }
+    
+    /**
+     * Default action for controller.
+     * 
+     * @param array $parameters
+     * 
+     * @author theKindlyMallard <the.kindly.mallard@gmail.com>
+     */
+    public function action_index(array $parameters = []) {
+        $this->outputHeader();
+        $this->outputView('index');
+        $this->outputFooter();
+    }
+    
+    public function action_add(array $parameters = []) {
+        
+        if (!UserController::isLoggedIn()) {
+            //Not logged in. Redirect login acction.
+            header('Location: ' . APPLICATION_URL . 'user' . DS . 'login');
+        }
+        
+        $this->checkPost();
+        
+        $gameTypes = (array)$this->model->getGameType();
+        
+        $this->outputHeader();
+//        $this->outputView('add');
+        require DIR_VIEW . strtolower($this->name) . DS . 'add' . FILE_PHTML;
+        $this->outputFooter();
+    }
+
+    /**
+     * Method to check $_POST and call proper actions.
+     * 
+     * @author theKindlyMallard <the.kindly.mallard@gmail.com>
+     */
+    private function checkPost() {
+        
+        if (!is_null(filter_input(INPUT_POST, self::FORM_NAME_GAME_ADD))) {
+            $this->dealWithAddNewGame();
+        }
+    }
+    
+    /**
+     * Prepares game data to save, call image uploading.
+     * 
+     * @author theKindlyMallard <the.kindly.mallard@gmail.com>
+     */
+    private function dealWithAddNewGame() {
+        
+        $imagePathFromRoot = str_replace('\\', '\\\\', str_replace(DIR_ROOT, '', $this->uploadGameImage()));
+        
+        $gameData = [
+            self::FIELD_COMPLEXITY => filter_input(INPUT_POST, self::FIELD_COMPLEXITY),
+            self::FIELD_DESCRIPTION => filter_input(INPUT_POST, self::FIELD_DESCRIPTION),
+            self::FIELD_IMAGE => $imagePathFromRoot,
+            self::FIELD_NAME => filter_input(INPUT_POST, self::FIELD_NAME),
+            self::FIELD_PLAYERS_NUMBER => filter_input(INPUT_POST, self::FIELD_PLAYERS_NUMBER),
+            self::FIELD_PLAY_TIME => filter_input(INPUT_POST, self::FIELD_PLAY_TIME),
+            self::FIELD_PUBLISHER => filter_input(INPUT_POST, self::FIELD_PUBLISHER),
+            self::FIELD_SITE_URL => filter_input(INPUT_POST, self::FIELD_SITE_URL),
+            self::FIELD_TYPE => filter_input(INPUT_POST, self::FIELD_TYPE),
+        ];
+        
+        $result = $this->model->saveGame($gameData);
+        
+        if ($result > 0) {
+            $this->messages['ok'] = 'Game saved with ID ' . $result;
+        } else {
+            $this->messages['error'] = 'Some errors occured. Game not saved.';
+        }
+    }
+    
+    /**
+     * Gets file data from sent form and try to upload it on server.
+     * 
+     * @return string Path to uploaded image or empty string if not uploaded.
+     * 
+     * @author theKindlyMallard <the.kindly.mallard@gmail.com>
+     */
+    private function uploadGameImage() {
+        
+        $target_file = DIR_IMAGES_GAMES . basename($_FILES[self::FIELD_IMAGE]['name']);
+        $uploadOk = 1;
+
+        $check = getimagesize($_FILES[self::FIELD_IMAGE]['tmp_name']);
+        if($check !== false) {
+            //"File is an image - " . $check['mime'] . ".";
+            $uploadOk = 1;
+        } else {
+            //"File is not an image.";
+            $uploadOk = 0;
+        }
+        // Check if $uploadOk is set to 0 by an error
+        if ($uploadOk == 0) {
+            //echo "Sorry, your file was not uploaded.";
+            $picturePath = '';
+        // if everything is ok, try to upload file
+        } else {
+            if (move_uploaded_file($_FILES[self::FIELD_IMAGE]['tmp_name'], $target_file)) {
+                //"The file ". basename( $_FILES["picture"]["name"]). " has been uploaded.";
+                $picturePath = $target_file;
+            } else {
+                //"Sorry, there was an error uploading your file.";
+                $picturePath = '';
+            }
+        }
+        
+        return $picturePath;
+    }
+}
---
+++ application/Model/GameModel.php
@@ -0,0 +1,181 @@
+<?php
+namespace Model;
+/**
+ * Model of game.
+ * 
+ * @author theKindlyMallard <the.kindly.mallard@gmail.com>
+ */
+class GameModel extends Model {
+    /**
+     * @var string Name of column complexity.
+     */
+    const COLUMN_COMPLEXITY = 'complexity';
+    /**
+     * @var string Name of column description.
+     */
+    const COLUMN_DESCRIPTION = 'description';
+    /**
+     * @var string Name of column image.
+     */
+    const COLUMN_IMAGE = 'image';
+    /**
+     * @var string Name of column play time.
+     */
+    const COLUMN_PLAY_TIME = 'play_time';
+    /**
+     * @var string Name of column players number.
+     */
+    const COLUMN_PLAYERS_NUMBER = 'players_number';
+    /**
+     * @var string Name of column publisher.
+     */
+    const COLUMN_PUBLISHER = 'publisher';
+    /**
+     * @var string Name of column name.
+     */
+    const COLUMN_NAME = 'name';
+    /**
+     * @var string Name of column site URL.
+     */
+    const COLUMN_SITE_URL = 'site_url';
+    /**
+     * @var string Name of column type.
+     */
+    const COLUMN_TYPE = 'fk_type';
+    /**
+     * @var string Name of column user.
+     */
+    const COLUMN_USER = 'fk_user';
+    
+    /**
+     * Gets user by field value e.g. ID.
+     * 
+     * @param string $field Field name for which get game (column name).
+     * @param type $value Value to search for in field.
+     * @return null|stdClass|array[stdClass] Returns single object if specified ID given or
+     *              array of objects if no ID given or NULL if no results.
+     * 
+     * @author theKindlyMallard <the.kindly.mallard@gmail.com>
+     */
+    public function getGameByField(string $field, $value) {
+        
+        $pdo = $this->getConnection();
+        $sql = 'SELECT * FROM `' . DB_NAME . "`.`game` WHERE `$field` = '$value'";
+        
+        $statement = $pdo->prepare($sql);
+        $statement->execute();
+        $result = $statement->fetchAll(\PDO::FETCH_OBJ);
+
+        if (count($result) == 0) {
+            return null;
+        } else if (count($result) == 1) {
+            return array_shift($result);
+        } else {
+            return $result;
+        }
+    }
+    
+    /**
+     * Gets game type from DB.
+     * 
+     * @param int $id ID of game type to get. Leave set to 0 if want get all.
+     * @return null|stdClass|array[stdClass] Returns single object if specified ID given or
+     *              array of objects if no ID given or NULL if no results.
+     * 
+     * @author theKindlyMallard <the.kindly.mallard@gmail.com>
+     */
+    public function getGameType(int $id = 0) {
+        
+        $pdo = $this->getConnection();
+        $sql = 'SELECT * FROM `' . DB_NAME . '`.`game_type`';
+        $sql .= ($id > 0) ? " WHERE `id` = $id" : '';
+        
+        $statement = $pdo->prepare($sql);
+        $statement->execute();
+        $result = $statement->fetchAll(\PDO::FETCH_OBJ);
+
+        if (count($result) == 0) {
+            return null;
+        } else if (count($result) == 1) {
+            return array_shift($result);
+        } else {
+            return $result;
+        }
+    }
+    
+    /**
+     * Saves new game info.
+     * 
+     * @param array $gameData Game data to save.
+     * @return int|bool Game ID or FALSE on failure.
+     * 
+     * @todo Better validation.
+     * 
+     * @author theKindlyMallard <the.kindly.mallard@gmail.com>
+     */
+    public function saveGame(array $gameData) {
+        
+        if (\Controller\UserController::isLoggedIn()) {
+            $userId = $_SESSION[\Controller\UserController::SESSION_KEY_USER_DATA]['id'];
+        } else {
+            return false;
+        }
+        
+        $gameDataValues = [
+            self::COLUMN_COMPLEXITY => $gameData[\Controller\GameController::FIELD_COMPLEXITY],
+            self::COLUMN_DESCRIPTION => $gameData[\Controller\GameController::FIELD_DESCRIPTION],
+            self::COLUMN_IMAGE => $gameData[\Controller\GameController::FIELD_IMAGE],
+            self::COLUMN_NAME => $gameData[\Controller\GameController::FIELD_NAME],
+            self::COLUMN_PLAYERS_NUMBER => $gameData[\Controller\GameController::FIELD_PLAYERS_NUMBER],
+            self::COLUMN_PLAY_TIME => $gameData[\Controller\GameController::FIELD_PLAY_TIME],
+            self::COLUMN_PUBLISHER => $gameData[\Controller\GameController::FIELD_PUBLISHER],
+            self::COLUMN_SITE_URL => $gameData[\Controller\GameController::FIELD_SITE_URL],
+            self::COLUMN_TYPE => $gameData[\Controller\GameController::FIELD_TYPE],
+            self::COLUMN_USER => $userId,
+        ];
+        
+        return $this->saveGameData($gameDataValues);
+        
+    }
+    
+    /**
+     * Saves game into DB.
+     * 
+     * @param array $gameData Game data to save.
+     * @return int|bool Saved game ID or FALSE on failure.
+     * 
+     * @author theKindlyMallard <the.kindly.mallard@gmail.com>
+     */
+    private function saveGameData(array $gameData) {
+        
+        $pdo = $this->getConnection();
+        
+        $sql = "INSERT INTO `" . DB_NAME . "`.`game` " . 
+            "(" 
+            . self::COLUMN_COMPLEXITY . ", " 
+            . self::COLUMN_DESCRIPTION . ", " 
+            . self::COLUMN_IMAGE . ", " 
+            . self::COLUMN_NAME . ", " 
+            . self::COLUMN_PLAYERS_NUMBER . ", " 
+            . self::COLUMN_PLAY_TIME . ", " 
+            . self::COLUMN_PUBLISHER . ", " 
+            . self::COLUMN_SITE_URL . ", " 
+            . self::COLUMN_TYPE . ", " 
+            . self::COLUMN_USER . " " 
+            .") " . 
+            "VALUES (" .
+            "'" . $gameData[self::COLUMN_COMPLEXITY] . "', " .
+            "'" . $gameData[self::COLUMN_DESCRIPTION] . "', " .
+            "'" . $gameData[self::COLUMN_IMAGE] . "', " .
+            "'" . $gameData[self::COLUMN_NAME] . "', " .
+            "'" . $gameData[self::COLUMN_PLAYERS_NUMBER] . "', " .
+            "'" . $gameData[self::COLUMN_PLAY_TIME] . "', " .
+            "'" . $gameData[self::COLUMN_PUBLISHER] . "', " .
+            "'" . $gameData[self::COLUMN_SITE_URL] . "', " .
+            "'" . $gameData[self::COLUMN_TYPE] . "', " .
+            "'" . $gameData[self::COLUMN_USER] . "'" .
+            ")";
+        
+        return $pdo->query($sql) ? $pdo->lastInsertId() : false;
+    }
+}
